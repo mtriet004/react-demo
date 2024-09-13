5318a166cdba720af2166b4d802cf6786cafb18f
mtriet004 <triet172004@gmail.com>
2024-09-13 21:53:03 +0700

    redux

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter} from 'react-router-dom'
 import Layout from './Layout';
-
+import store from './redux/store';
+import {Provider} from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </Provider>
+    
   // {/* </React.StrictMode> */}
 );
 
