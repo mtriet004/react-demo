4b54e5e18cd987e5e1f405edd750504d22934fd9
mtriet004 <triet172004@gmail.com>
2024-09-15 12:41:33 +0700

    video80

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducer/rootReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+ 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
-export default  store
+// let store = createStore(persistedReducer)
+let persistor = persistStore(store)
+
+export {store, persistor}
