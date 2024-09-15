c9b1e32390a3e8d8c003f29fd51461f4aed3757d
mtriet004 <triet172004@gmail.com>
2024-09-15 18:21:47 +0700

    video83

--- src/components/User/DetailQuiz.jsx
+++ src/components/User/DetailQuiz.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getDataQuiz } from '../../service/APIService'
-
+import _ from 'lodash'
 const DetailQuiz = () => {
 
   const param = useParams()
@@ -13,7 +13,28 @@ const DetailQuiz = () => {
 
   const fetchQuestion = async() =>{
     let res = await getDataQuiz(quizId)
-    // console.log(res)
+    console.log(res)
+    if(res && res.EC ===0){
+        let raw = res.DT
+        let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+            const answers = []
+            let questionDescription, image = null;
+            value.forEach((item, index) => {
+                if(index ===0){
+                    questionDescription = item.description
+                    image = item.image
+                }
+                answers.push(item.answers)
+            })
+            return {color: key, answers, questionDescription, image}
+        })
+        .value()
+        console.log(data)
+    }
   }
   console.log('check param', param)  
   return (
